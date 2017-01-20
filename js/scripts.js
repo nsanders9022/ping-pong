de990917bf6380c4eff313a3b0a90391482a7dbb
Nicole Sanders <nsanders9022@gmail.com>
2017-01-20 08:15:33 -0800

    display the numbers from 1 up to and including the user input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+var pingPong = function(number) {
+  var outputArray = []
+
+  for (i = 1; i <= number; i++) {
+    outputArray.push(i);
+  }
+
+  return outputArray
+}
+
+
+
+
 $(document).ready(function(){
-  
+  $("#ping-form").submit(function(event){
+    event.preventDefault();
+
+    var userInput = parseInt($("#number").val());
+
+    $("#result").show();
+    $("#output").text(pingPong(userInput))
+
+  })
 });
